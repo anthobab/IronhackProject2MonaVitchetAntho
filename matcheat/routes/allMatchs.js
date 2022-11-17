657c98704c37ad63b94f0f6c7f7f47badb543c04
Vitchet Uk <vitchet.uk@hotmail.com>
2022-11-17 17:13:36 +0100

    need to work on all matchs

--- matcheat/routes/allMatchs.js
+++ matcheat/routes/allMatchs.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const Match = require("../models/Match.model");
-
+const Meeting = require("../models/Meetings.model");
 //To access Match-list page//
 
 router.get("/all-matchs", async (req, res) => {
   const userId = req.session.currentUser;
   console.log(userId);
-  const filteredMatchs = await Match.find({
+  const filteredMatchs = await Meeting.find({
     $or: [{ matcher: userId }, { matchee: userId }],
-  });
+  }).populate("users");
   console.log(filteredMatchs);
+  console.log(filteredMatchs[0].users);
   res.render("all-matchs", { allMatchs: filteredMatchs });
 });
 
