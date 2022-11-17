0d24dfa779ab7f38c2d9bfb42eb203b158e09e33
Vitchet Uk <vitchet.uk@hotmail.com>
2022-11-17 18:45:51 +0100

    allmatch finished (i hope)

--- matcheat/routes/allMatchs.js
+++ matcheat/routes/allMatchs.js
@@ -2,16 +2,33 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const Match = require("../models/Match.model");
 const Meeting = require("../models/Meetings.model");
+const mongoose = require("mongoose");
 //To access Match-list page//
 
 router.get("/all-matchs", async (req, res) => {
-  const userId = req.session.currentUser;
+  const userId = new mongoose.Types.ObjectId(res.locals.currentUser._id);
   console.log(userId);
   const filteredMatchs = await Meeting.find({
-    $or: [{ matcher: userId }, { matchee: userId }],
+    users: {
+      $in: [userId],
+    },
   }).populate("users");
   console.log(filteredMatchs);
   console.log(filteredMatchs[0].users);
+  filteredMatchs.forEach((meetingEl) => {
+    meetingEl.users.map((userEl) => {
+      //console.log(
+      //  "\n\n\n compare",
+      //  userEl.id,
+      //  res.locals.currentUser._id,
+      //  userEl.id === res.locals.currentUser._id
+      //);
+      userEl.isNotMe = userEl.id !== res.locals.currentUser._id;
+      return userEl;
+    });
+  });
+
+  console.log(filteredMatchs[0]);
   res.render("all-matchs", { allMatchs: filteredMatchs });
 });
 
