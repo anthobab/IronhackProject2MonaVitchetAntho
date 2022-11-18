43ac5c1e6c68648c5bafe8ecdf8cc359f5f8713a
anthobab <anthony.babin1@gmail.com>
2022-11-18 08:32:22 +0100

    try catch in route

--- matcheat/routes/allMatchs.js
+++ matcheat/routes/allMatchs.js
@@ -6,23 +6,27 @@ const mongoose = require("mongoose");
 //To access Match-list page//
 
 router.get("/all-matchs", async (req, res) => {
-  const userId = new mongoose.Types.ObjectId(res.locals.currentUser._id);
+  try {
+    const userId = new mongoose.Types.ObjectId(res.locals.currentUser._id);
 
-  const filteredMatchs = await Meeting.find({
-    users: {
-      $in: [userId],
-    },
-  }).populate("users");
+    const filteredMatchs = await Meeting.find({
+      users: {
+        $in: [userId],
+      },
+    }).populate("users");
 
-  filteredMatchs.forEach((meetingEl) => {
-    meetingEl.users.map((userEl) => {
-      userEl.isNotMe = userEl.id !== res.locals.currentUser._id;
-      return userEl;
+    filteredMatchs.forEach((meetingEl) => {
+      meetingEl.users.map((userEl) => {
+        userEl.isNotMe = userEl.id !== res.locals.currentUser._id;
+        return userEl;
+      });
     });
-  });
 
-  console.log(filteredMatchs[0]);
-  res.render("all-matchs", { allMatchs: filteredMatchs });
+    console.log(filteredMatchs[0]);
+    res.render("all-matchs", { allMatchs: filteredMatchs });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
